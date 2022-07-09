9f131e1da898554a2aef7bb3a17bcf1b75df5500
Adrian Ciura <aciura@gmail.com>
2022-07-09 11:56:44 +0200

    Change background gradient

--- ui/src/theme/theme.tsx
+++ ui/src/theme/theme.tsx
@@ -6,7 +6,7 @@ export const theme = extendTheme({
     global: (props: StyleFunctionProps) => ({
       body: {
         bgGradient: mode(
-          'linear(to-r, blue.50 0%, blue.100 25%, pink.50 100%)',
+          'linear(to-r, blue.50 0%, purple.200 25%, pink.100 100%)',
           'linear(to-r, gray.700 0%, gray.800 50%, blue.900 100%)',
         )(props),
       },
